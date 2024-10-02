e704663b42a19a5e9410dc377d9c029f3be5982f
Tommytinny <tommyteenboi@gmail.com>
2024-10-02 17:40:26 +0100

    fix errors in the file

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // eslint-disable-next-line guard-for-in
+  const newArray = [];
   for (const idx in array) {
     const value = array[idx];
-    // eslint-disable-next-line no-param-reassign
-    array[idx] = appendString + value;
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
